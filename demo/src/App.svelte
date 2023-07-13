4ba3f6f6499af3b4122133d2a606126b1f37a219
杜府 <dufu1991@163.com>
2023-07-13 14:45:24 +0800

    Fix auto deploy demo workflow

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -3,6 +3,7 @@
     import Router, { querystring, location, push } from 'svelte-spa-router';
     import { routes, routes_en } from './route';
     import { NavBar, Icon } from '../../components';
+    
     import zh_CN from 'stdf/lang/zh_CN';
     import en_US from 'stdf/lang/en_US';
     import menuList from './data/menuList';
