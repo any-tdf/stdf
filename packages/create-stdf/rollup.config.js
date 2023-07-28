4ccc6e3b04bcbe034e86709758999e79695429a9
杜府 <dufu1991@163.com>
2023-07-29 02:11:06 +0800

    Update cerate-stdf pack

--- packages/create-stdf/rollup.config.js
+++ packages/create-stdf/rollup.config.js
@@ -2,10 +2,7 @@ import terser from '@rollup/plugin-terser';
 
 export default {
     input: 'src/index.js',
-    output: [
-        { file: 'dist/index.js', format: 'es' },
-        { file: 'dist/index.mjs', format: 'esm' },
-    ],
+    output: [{ file: 'dist/index.js', format: 'es' }],
     external: ['path', 'fs-extra', '@clack/prompts', 'kleur/colors'],
     plugins: [terser()],
 };
