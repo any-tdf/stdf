6a296ece824ba624936fba98c047ee651ba85401
dufu1991 <dufu1991@163.com>
2023-08-16 15:31:35 +0800

    [create]Add description

--- packages/create-stdf/templates/vite-uno/vite.config.js
+++ packages/create-stdf/templates/vite-uno/vite.config.js
@@ -26,6 +26,12 @@ export default defineConfig({
                 defaultHandler(warning);
             },
         }),
+        // 一般来说，一个应用的所有 svg 都放在一个文件夹下，合并为一个 symbol 即可。
+        // 如默认配置的 src/assets/svgs 内，合并之后 symbol 会放置于 public/fonts/symbol.svg 下。
+        // 此处只是为了演示不同文件夹下的图标如何合并为不同的 symbol。
+        // Generally speaking, all of an application svg are placed in a folder and merged into one symbol.
+        // As in the default configuration of src/assets/svgs, the merged symbol will be placed under public/fonts/symbol.svg.
+        // Here is just to show how icons in different folders are merged into different symbols.
         svgSprite([
             { inFile: 'src/assets/Heroicons', outFile: 'public/fonts', fileName: 'Heroicons' },
             { inFile: 'src/assets/IconPark', outFile: 'public/fonts', fileName: 'IconPark' },
