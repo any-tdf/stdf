311f70a0841de39eb0eceaf64691fe89d4768983
dufu1991 <dufu1991@163.com>
2023-08-28 11:31:00 +0800

    [create]Ignore svelte 'a11y-' warning

--- packages/create-stdf/templates/vite-uno/vite.config.js
+++ packages/create-stdf/templates/vite-uno/vite.config.js
@@ -12,15 +12,9 @@ export default defineConfig({
         }),
         svelte({
             onwarn(warning, defaultHandler) {
-                const ignoredWarnings = [
-                    'a11y-distracting-elements',
-                    'a11y-no-static-element-interactions',
-                    'a11y-click-events-have-key-events',
-                    'a11y-no-noninteractive-element-interactions',
-                ];
-                const { code } = warning;
-                // don't warn on <marquee> elements, cos they're cool
-                if (ignoredWarnings.includes(code)) return;
+                if (warning.code.startsWith('a11y-')) {
+                    return;
+                }
 
                 // handle all other warnings normally
                 defaultHandler(warning);
