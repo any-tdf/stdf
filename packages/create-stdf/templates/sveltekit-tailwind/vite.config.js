32071558c9c4bbd613dff89394218240540a0a7e
dufu1991 <dufu1991@163.com>
2023-11-13 10:25:32 +0800

    [create]Update for rollup-plugin-stdf-icon 0.0.13.

--- packages/create-stdf/templates/sveltekit-tailwind/vite.config.js
+++ packages/create-stdf/templates/sveltekit-tailwind/vite.config.js
@@ -4,18 +4,17 @@ import svgSprite from 'rollup-plugin-stdf-icon';
 
 export default defineConfig({
 	plugins: [
+		// @ts-ignore
 		sveltekit({}),
-		// 一般来说，一个应用的所有 svg 都放在一个文件夹下，合并为一个 symbol 即可。
-		// 此处只是为了演示不同文件夹下的图标如何合并为不同的 symbol。
-		// Generally speaking, all of an application svg are placed in a folder and merged into one symbol.
-		// Here is just to show how icons in different folders are merged into different symbols.
+		// 一般来说，一个应用的的 svg 不多时都放在一个文件夹下，合并为一个 symbol 即可。此处演示了不同文件夹下的图标如何合并为不同的 symbol。
+		// Generally speaking, when the svg of an application is not much, it is placed in a folder and merged into one symbol. This example shows how the icons in different folders are merged into different symbols.
 		svgSprite([
-			{ inFile: 'src/lib/svgs/Heroicons', outFile: 'static/symbols', fileName: 'Heroicons' },
-			{ inFile: 'src/lib/svgs/IconPark', outFile: 'static/symbols', fileName: 'IconPark' },
-			{ inFile: 'src/lib/svgs/Remix', outFile: 'static/symbols', fileName: 'Remix' },
+			{ inFile: 'src/lib/svgs/Heroicons', outFile: 'static/symbols' },
+			{ inFile: 'src/lib/svgs/IconPark', outFile: 'static/symbols' },
+			{ inFile: 'src/lib/svgs/Remix', outFile: 'static/symbols' },
 			// 单独为 Cell 与 NavBar 用到的图标创建一个 symbol。详见：https://stdf.design/#/guide?nav=icon
 			// Create a symbol for the icons used by Cell and NavBar. See: https://stdf.design/#/guide?nav=icon
-			{ inFile: 'src/lib/icons', outFile: 'static/fonts', fileName: 'symbol' },
+			{ inFile: 'src/lib/symbol', outFile: 'static/fonts' },
 		]),
 	],
 });
--- packages/create-stdf/templates/vite-uno/vite.config.js
+++ packages/create-stdf/templates/vite-uno/vite.config.js
@@ -6,7 +6,9 @@ import svgSprite from 'rollup-plugin-stdf-icon';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// @ts-ignore
 	plugins: [
+		// @ts-ignore
 		UnoCSS({
 			extractors: [extractorSvelte()],
 		}),
@@ -20,16 +22,12 @@ export default defineConfig({
 				defaultHandler(warning);
 			},
 		}),
-		// 一般来说，一个应用的所有 svg 都放在一个文件夹下，合并为一个 symbol 即可。
-		// 如默认配置的 src/assets/icons 内，合并之后 symbol 会放置于 public/fonts/symbol.svg 下。
-		// 此处只是为了演示不同文件夹下的图标如何合并为不同的 symbol。
-		// Generally speaking, all of an application svg are placed in a folder and merged into one symbol.
-		// As in the default configuration of src/assets/icons, the merged symbol will be placed under public/fonts/symbol.svg.
-		// Here is just to show how icons in different folders are merged into different symbols.
+		// 一般来说，一个应用的的 svg 不多时都放在一个文件夹下，合并为一个 symbol 即可。此处演示了不同文件夹下的图标如何合并为不同的 symbol。
+		// Generally speaking, when the svg of an application is not much, it is placed in a folder and merged into one symbol. This example shows how the icons in different folders are merged into different symbols.
 		svgSprite([
-			{ inFile: 'src/assets/Heroicons', outFile: 'public/fonts', fileName: 'Heroicons' },
-			{ inFile: 'src/assets/IconPark', outFile: 'public/fonts', fileName: 'IconPark' },
-			{ inFile: 'src/assets/Remix', outFile: 'public/fonts', fileName: 'Remix' },
+			{ inFile: 'src/assets/Heroicons', outFile: 'public/fonts' },
+			{ inFile: 'src/assets/IconPark', outFile: 'public/fonts' },
+			{ inFile: 'src/assets/Remix', outFile: 'public/fonts' },
 		]),
 	],
 });
