48dd40296ee2bf03fe104d2e86f8daad5a3eea61
shynome <shynome@gmail.com>
2024-08-01 20:04:53 +0800

    [stdf]Add base TypeScript support

---
+++ packages/stdf/components/index.d.ts
@@ -0,0 +1,93 @@
+import ActionSheet from './actionSheet/ActionSheet.svelte';
+import AsyncPicker from './asyncPicker/AsyncPicker.svelte';
+import Avatar from './avatar/Avatar.svelte';
+import Avatars from './avatar/Avatars.svelte';
+import Badge from './badge/Badge.svelte';
+import BottomSheet from './bottomSheet/BottomSheet.svelte';
+import Button from './button/Button.svelte';
+import Calendar from './calendar/Calendar.svelte';
+import Cell from './cell/Cell.svelte';
+import CellGroup from './cell/CellGroup.svelte';
+import Checkbox from './checkbox/Checkbox.svelte';
+import CheckboxGroup from './checkbox/CheckboxGroup.svelte';
+import Dialog from './dialog/Dialog.svelte';
+import Divider from './divider/Divider.svelte';
+import Grid from './grids/Grid.svelte';
+import Grids from './grids/Grids.svelte';
+import Icon from './icon/Icon.svelte';
+import IndexBar from './indexBar/IndexBar.svelte';
+import Input from './input/Input.svelte';
+import Loading from './loading/Loading.svelte';
+import Mask from './mask/Mask.svelte';
+import Modal from './modal/Modal.svelte';
+import NavBar from './navBar/NavBar.svelte';
+import NoticeBar from './noticeBar/NoticeBar.svelte';
+import NumKeyboard from './numKeyboard/NumKeyboard.svelte';
+import Pagination from './pagination/Pagination.svelte';
+import Picker from './picker/Picker.svelte';
+import Placeholder from './placeholder/Placeholder.svelte';
+import Popup from './popup/Popup.svelte';
+import Progress from './progress/Progress.svelte';
+import ProgressLoop from './progressLoop/ProgressLoop.svelte';
+import Radio from './radio/Radio.svelte';
+import RadioGroup from './radio/RadioGroup.svelte';
+import Rate from './rate/Rate.svelte';
+import Skeleton from './skeleton/Skeleton.svelte';
+import Slider from './slider/Slider.svelte';
+import Steps from './steps/Steps.svelte';
+import Switch from './switch/Switch.svelte';
+import Swiper from './swiper/Swiper.svelte';
+import Tab from './tabs/Tab.svelte';
+import TabBar from './tabBar/TabBar.svelte';
+import TabContent from './tabs/TabContent.svelte';
+import Tabs from './tabs/Tabs.svelte';
+import TimePicker from './timePicker/TimePicker.svelte';
+import Toast from './toast/Toast.svelte';
+
+export {
+	ActionSheet,
+	AsyncPicker,
+	Avatar,
+	Avatars,
+	Badge,
+	BottomSheet,
+	Button,
+	Calendar,
+	Cell,
+	CellGroup,
+	Checkbox,
+	CheckboxGroup,
+	Dialog,
+	Divider,
+	Grid,
+	Grids,
+	Icon,
+	IndexBar,
+	Input,
+	Loading,
+	Mask,
+	Modal,
+	NavBar,
+	NoticeBar,
+	NumKeyboard,
+	Pagination,
+	Picker,
+	Placeholder,
+	Popup,
+	Progress,
+	ProgressLoop,
+	Radio,
+	RadioGroup,
+	Rate,
+	Skeleton,
+	Slider,
+	Steps,
+	Switch,
+	Swiper,
+	Tab,
+	TabBar,
+	TabContent,
+	Tabs,
+	TimePicker,
+	Toast,
+};
