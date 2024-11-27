c9109c967cd0ef49abbda54fbff825af4665a45d
dufu1991 <dufu1991@163.com>
2024-11-28 05:59:46 +0800

    [stdf]Update Swiper compoent, demo and documents.

--- demo/src/routes/zh_CN/swiper/Item.svelte
+++ demo/src/routes/zh_CN/swiper/Item.svelte
@@ -4,10 +4,6 @@
 
 <div class="flex flex-col space-y-4 items-center">
 	<div class="text-center">此容器使用单个组件作为内容</div>
-	<div>
-		<Avatar />
-	</div>
-	<div class="w-full">
-		<Button>按 钮</Button>
-	</div>
+	<div><Avatar /></div>
+	<div class="w-full"><Button>按钮</Button></div>
 </div>
