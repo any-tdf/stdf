8966ea6dec1aa4883852539acfea6f4513adf14f
dufu1991 <dufu1991@163.com>
2024-12-01 00:52:54 +0800

    [docs]Update site

--- site/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-import tailwind from 'tailwindcss';
-import tailwindConfig from './tailwind.config.cjs';
-import autoprefixer from 'autoprefixer';
-
-export default {
-	plugins: [tailwind(tailwindConfig), autoprefixer],
-};
