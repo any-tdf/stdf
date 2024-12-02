4f81ae2de7a0faef21ec6aabcbecee390af07170
dufu1991 <dufu1991@163.com>
2024-12-02 10:37:02 +0800

    [stdf]Add ar_EG language

---
+++ packages/stdf/src/lib/lang/ar_EG.ts
@@ -0,0 +1,83 @@
+import type { LangProps } from './index.js';
+
+const ar_EG: LangProps = {
+	common: {
+		slotEmpty: 'محتويات الفتحة فارغة!',
+		loading: 'جار التحميل ...',
+		noData: 'لا يوجد بيانات',
+		noMoreData: 'لا توجد بيانات إضافية',
+		done: 'تم'
+	},
+	actionSheet: {
+		cancelText: 'إلغاء'
+	},
+	bottomSheet: {
+		title: 'العنوان'
+	},
+	dialog: {
+		title: 'تلميح',
+		content: 'المحتوى',
+		primaryText: 'تأكيد',
+		secondaryText: 'إلغاء'
+	},
+	input: {
+		pleaseInput: 'الرجاء إدخال'
+	},
+	modal: {
+		title: 'العنوان',
+		content: 'المحتوى',
+		btnText: 'أراهن'
+	},
+	navBar: {
+		title: 'العنوان'
+	},
+	pagination: {
+		defaultOnlyOnePage: 'فقط صفحة واحدة'
+	},
+	picker: {
+		defaultTitle: 'الرجاء الاختيار',
+		defaultCancel: 'إلغاء',
+		defaultConfirm: 'تأكيد'
+	},
+	asyncPicker: {
+		defaultTitle: 'الرجاء الاختيار',
+		defaultCancel: 'إلغاء',
+		defaultConfirm: 'تأكيد',
+		defaultNext: 'التالي',
+		defaultPrev: 'السابق',
+		defaultSelected: 'المختار'
+	},
+	rate: {
+		error1: 'لا يمكن أن يكون القيمة أكبر من الإجمالي!',
+		error2: 'يجب أن يتم إزالة الإجمالي بمقدار 1!',
+		error3: 'يجب أن تكون القيمة أقل بمقدار 1 عندما لا تكون مختارة نصفياً!',
+		error4: 'يجب أن يتم إزالة القيمة بمقدار 0.5 عند الاختيار بشكل نصفي!',
+		error5: 'العرض أو الارتفاع أقل من 20، والذي يجعل النقر على التليفون المحمول صعبًا!'
+	},
+	timePicker: {
+		defaultTitle: 'الرجاء الاختيار',
+		defaultCancel: 'إلغاء',
+		defaultConfirm: 'تأكيد',
+		defaultYear: 'سنة',
+		defaultMonth: 'شهر',
+		defaultDay: 'يوم',
+		defaultHour: 'ساعة',
+		defaultMinute: 'دقيقة',
+		defaultSecond: 'ثانية'
+	},
+	calendar: {
+		weekTextList: ['الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت', 'الأحد'],
+		weekSundayStartTextList: ['الأحد', 'الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
+		monthTextList: ['يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
+		confirmText: 'تأكيد',
+		selectedText: 'المحددة',
+		dayText: 'يوم',
+		beforeText: 'الأخيرة',
+		afterText: 'المستقبل',
+		currentWeekText: 'هذا الأسبوع',
+		currentMonthText: 'هذا الشهر',
+		currentQuarterText: 'هذه الفصل'
+	}
+};
+
+export default ar_EG;
