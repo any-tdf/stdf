3949b180f445e4b2ead41bb171994c709ae67eb3
dufu1991 <dufu1991@163.com>
2024-12-13 15:43:32 +0800

    [demo]build for cloudflare

--- packages/stdf/svelte.config.js
+++ packages/stdf/svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { Config } from '@sveltejs/kit';
 
-const config: Config = {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
