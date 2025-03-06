2fc44194ad58e69c32c76fe751cb27e422f38c75
dufu1991 <dufu1991@163.com>
2025-03-06 17:45:56 +0800

    [stdf]Update Tailwind v4 config

--- packages/stdf/vite.config.ts
+++ packages/stdf/vite.config.ts
@@ -1,8 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import svgSprite from 'rollup-plugin-stdf-icon';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import svgSprite from "rollup-plugin-stdf-icon";
 
 export default defineConfig({
-	plugins: [sveltekit(), svgSprite([{ inFile: 'src/assets/svgs', outFile: 'static/fonts', fileName: 'symbol' }])],
-	server: { hmr: true, host: '0.0.0.0', port: 8888 }
+	plugins: [
+		sveltekit(),
+		tailwindcss(),
+		svgSprite([
+			{
+				inFile: "src/assets/svgs",
+				outFile: "static/fonts",
+				fileName: "symbol",
+			},
+		]),
+	],
+	server: { hmr: true, host: "0.0.0.0", port: 8888 },
 });
