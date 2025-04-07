08c15fa6cf2436ae118c7e562823933bc742b94c
dufu1991 <dufu1991@163.com>
2025-04-07 09:41:54 +0800

    [create]更新脚本以使用node:child_process模块，调整命令输出信息格式，并简化模板路径更新逻辑

--- packages/create-stdf/scripts/update.js
+++ packages/create-stdf/scripts/update.js
@@ -1,5 +1,5 @@
 import fs from 'node:fs';
-import { exec } from 'child_process';
+import { exec } from 'node:child_process';
 
 // 封装函数，传入版本号，需要修改的 package.json 中的 devDependencies 的 key 值，需要修改的文件路径，直接修改 package.json 并重新写入
 // Encapsulate the function, pass in the version number, the key value of devDependencies in package.json that needs to be modified, the file path that needs to be modified, and directly modify package.json and rewrite
@@ -12,42 +12,38 @@ const updatePackageJson = (version, key, path) => {
 
 // 获取 stdf 最新版本
 // Get the latest version of stdf
-exec(`npm show stdf version`, (error, stdout, stderr) => {
+exec('npm show stdf version', (error, stdout, stderr) => {
 	if (error) {
-		console.error(`执行命令出错: ${error.message}`);
+		console.error(`执行命令出错：${error.message}`);
 		return;
 	}
 	if (stderr) {
-		console.error(`命令错误: ${stderr}`);
+		console.error(`命令错误：${stderr}`);
 		return;
 	}
 	const version = stdout.trim();
-	console.log(`👉 stdf 最新版本: ${version}`);
+	console.log(`👉 stdf 最新版本：${version}`);
 	// 更新模板中的 package.json 的 stdf 版本号
 	// Update the stdf version number in the template package.json
-	updatePackageJson(version, 'stdf', 'templates/vite-tailwind/package.json');
-	updatePackageJson(version, 'stdf', 'templates/vite-uno/package.json');
-	updatePackageJson(version, 'stdf', 'templates/sveltekit-tailwind/package.json');
-	updatePackageJson(version, 'stdf', 'templates/sveltekit-uno/package.json');
+	updatePackageJson(version, 'stdf', 'templates/skt/package.json');
+	updatePackageJson(version, 'stdf', 'templates/sktt/package.json');
 });
 
 // 获取 rollup-plugin-stdf-icon 最新版本
 // Get the latest version of rollup-plugin-stdf-icon
-exec(`npm show rollup-plugin-stdf-icon version`, (errorIcon, stdoutIcon, stderrIcon) => {
+exec('npm show rollup-plugin-stdf-icon version', (errorIcon, stdoutIcon, stderrIcon) => {
 	if (errorIcon) {
-		console.error(`执行命令出错: ${errorIcon.message}`);
+		console.error(`执行命令出错：${errorIcon.message}`);
 		return;
 	}
 	if (stderrIcon) {
-		console.error(`命令错误: ${stderrIcon}`);
+		console.error(`命令错误：${stderrIcon}`);
 		return;
 	}
 	const version = stdoutIcon.trim();
-	console.log(`👉 rollup-plugin-stdf-icon 最新版本: ${version}`);
+	console.log(`👉 rollup-plugin-stdf-icon 最新版本：${version}`);
 	// 更新模板中的 package.json 的 rollup-plugin-stdf-icon 版本号
 	// Update the rollup-plugin-stdf-icon version number in the template package.json
-	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/vite-tailwind/package.json');
-	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/vite-uno/package.json');
-	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/sveltekit-tailwind/package.json');
-	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/sveltekit-uno/package.json');
+	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/skt/package.json');
+	updatePackageJson(version, 'rollup-plugin-stdf-icon', 'templates/sktt/package.json');
 });
