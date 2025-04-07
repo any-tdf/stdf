35e001ba5b15b2935a576081c0aa36c3c1655bca
dufu1991 <dufu1991@163.com>
2025-04-07 17:57:59 +0800

    [stdf]更新svelte.config.js以支持404页面回退和动态基础路径配置；优化vite.config.ts中的插件配置；新增GitHub Actions工作流以自动部署Demo静态内容至GitHub Pages。

--- packages/stdf/svelte.config.js
+++ packages/stdf/svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
--- packages/stdf/vite.config.ts
+++ packages/stdf/vite.config.ts
@@ -1,19 +1,9 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import svgSprite from "rollup-plugin-stdf-icon";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import svgSprite from 'rollup-plugin-stdf-icon';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		tailwindcss(),
-		svgSprite([
-			{
-				inFile: "src/assets/svgs",
-				outFile: "static/fonts",
-				fileName: "symbol",
-			},
-		]),
-	],
-	server: { hmr: true, host: "0.0.0.0", port: 8888 },
+	plugins: [sveltekit(), tailwindcss(), svgSprite([{ inFile: 'src/assets/svgs', outFile: 'static/fonts', fileName: 'symbol' }])],
+	server: { hmr: true, host: '0.0.0.0', port: 8888 }
 });
