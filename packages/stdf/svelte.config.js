5b1dd7c4c5ade2b1647a8db4de9324995a00087b
dufu1991 <dufu1991@163.com>
2025-04-08 09:26:45 +0800

    [demo] 更新依赖项，替换为@ sveltejs/adapter-static，调整svelte.config.js以支持静态生成，并启用客户端渲染和预渲染功能。

--- packages/stdf/svelte.config.js
+++ packages/stdf/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
+		adapter: adapter({ pages: 'build', assets: 'build', fallback: '404.html', precompress: false, strict: true }),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
