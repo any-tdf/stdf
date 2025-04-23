d1a2c35a95ab586303f9aa5ef4c1cb35e3dec2cb
dufu1991 <dufu1991@163.com>
2025-04-23 17:39:41 +0800

    [doc]更新svelte.config.js文件，修改适配器配置以支持404页面回退。

--- docs/site/svelte.config.js
+++ docs/site/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({ fallback: '404.html' })
 	}
 };
 
